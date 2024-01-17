926cf66b49870709093c339c6ea82c15e68c54de
Mayur <mayuravhad333@gmail.com>
2024-01-18 01:16:46 +0530

    first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "We are living in Kohlination" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('We are living in Kohlination');
+ res.send('Good Night Jhopa aata sgle');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
