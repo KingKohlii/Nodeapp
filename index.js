b3083b3d9d77f7315413aee0fcf5d19a303c0bec
Mayur <mayuravhad333@gmail.com>
2024-01-18 17:33:39 +0530

    first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();//Respond with "hello world" for requests that hit our root>
 app.get('/', function (req, res) {
- res.send('Good Morning have a nice day');
+ res.send('Good Evening');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
 
